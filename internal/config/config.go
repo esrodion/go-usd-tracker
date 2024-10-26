12424f7f4dab06e0802e1545680e22837769fb6f
letr.rod <letr.rod@gmail.com>
2024-10-26 15:50:01 +0300

    Read environment variables into config file

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v6"
+)
+
+type Config struct {
+	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"0.0.0.0:8080"`
+	LogLevel      string `env:"LOG_LEVEL" envDefault:"DEBUG"`
+	PostgresConfig
+}
+
+func NewConfig() (*Config, error) {
+	config := &Config{}
+
+	err := env.Parse(config)
+	if err != nil {
+		err = fmt.Errorf("config.NewConfig: %w", err)
+	}
+	return config, err
+}
+
+type PostgresConfig struct {
+	Conn            string `env:"POSTGRES_CONN" envDefault:"postgres://test:test@db:5432/test?sslmode=disable"`
+	AutoMigrateUp   string `env:"AUTO_MIGRATE_UP" envDefault:"true"`
+	AutoMigrateDown string `env:"AUTO_MIGRATE_DOWN" envDefault:"false"`
+	MigrationsURL   string `env:"MIGRATIONS_URL" envDefault:"file:///goserver/internal/repository/db/migrations/"`
+}
+
+func NewPostgresConfig() (*PostgresConfig, error) {
+	config := &PostgresConfig{}
+
+	err := env.Parse(config)
+	if err != nil {
+		err = fmt.Errorf("config.NewPostgresConfig: %w", err)
+	}
+	return config, err
+}
